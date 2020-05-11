b192b95717526c5e8b68c067b4e3279ad7205287
delmasdev <delmas.dev@gmail.com>
2020-05-11 15:31:46 +0200

    sendgrid

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+
+gem 'dotenv-rails'
 gem 'faker'
 gem 'table_print'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -41,6 +43,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'letter_opener'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  after_create :welcome_send
+
   has_many :admin_events, foreign_key: 'admin_id', class_name: "Event"
   has_many :attendances
   has_many :events, through: :attendances
@@ -12,6 +14,11 @@ class User < ApplicationRecord
     format: {with: /\A[^@\s]+@([^@\s]+.)+[^@\s]+\z/, message: "email adress please"}
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 
 
 end
