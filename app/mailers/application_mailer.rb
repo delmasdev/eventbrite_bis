1d0ea9eed853b101d6646da219a135211619e97f
delmasdev <delmas.dev@gmail.com>
2020-05-11 17:04:14 +0200

    gmailv2

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -11,4 +11,5 @@ class ApplicationMailer < ActionMailer::Base
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   def welcome_send
-    UserMailer.welcome_email(self).deliver_now
+    ApplicationMailer.welcome_email(self).deliver_now
   end
 
 
