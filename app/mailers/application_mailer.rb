08aefdd1088626449e46cc2e310977216f8ea342
delmasdev <delmas.dev@gmail.com>
2020-05-11 16:55:06 +0200

    gmail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: 'pokjett@gmail.com'
+
+  def welcome_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://www.develo.fr/login'
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Bienvenue chez nous !')
+  end
 end
