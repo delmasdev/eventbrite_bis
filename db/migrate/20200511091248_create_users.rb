347d1a0c8a4258eef965520a71e8a465516ba63e
delmasdev <delmas.dev@gmail.com>
2020-05-11 12:56:56 +0200

    second

--- db/migrate/20200511091248_create_users.rb
+++ db/migrate/20200511091248_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :fist_name
+      t.string :first_name
       t.string :last_name
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :password_digest, null: false, default: ""
       t.text :description
       t.timestamps
     end
